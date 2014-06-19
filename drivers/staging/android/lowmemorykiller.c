326fb2510c316aef62841d5cfbaa3331d2cf837d
Paul Reioux <reioux@gmail.com>
2014-06-19 11:16:07 -0500

    lowmemorykiller: fix logic derp for ZRAM/ZSWAP enabled use cases
    instead of looking at the total available compressed swap, just look at the
    free swap pages and take a 50% buffer for it
    
    Change-Id: Ifce68eef7576bc9acfad37126c2c99834fd9c544
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -445,19 +445,6 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 	struct sysinfo si;
 #endif
 
-#ifdef CONFIG_ZRAM_FOR_ANDROID
-	other_file -= total_swapcache_pages;
-#endif
-
-	rcu_read_lock();
-	tsk = current->group_leader;
-	if ((tsk->flags & PF_EXITING) && test_task_flag(tsk, TIF_MEMDIE)) {
-		set_tsk_thread_flag(current, TIF_MEMDIE);
-		rcu_read_unlock();
-		return 0;
-	}
-	rcu_read_unlock();
-
 	if (nr_to_scan > 0) {
 		if (mutex_lock_interruptible(&scan_mutex) < 0)
 			return 0;
@@ -468,7 +455,7 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 	other_free = global_page_state(NR_FREE_PAGES);
 	other_file = global_page_state(NR_FILE_PAGES) -
 						global_page_state(NR_SHMEM) +
-						(si.totalswap >> 2) -
+						(si.freeswap >> 1) -
 						total_swapcache_pages;
 #else
 	other_free = global_page_state(NR_FREE_PAGES);
